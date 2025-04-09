70b2c8b14e1bf55cb1f31be5aaf6a34f435e5d9a
vktech04 <kulkarnivaishu9504@gmail.com>
2025-04-09 16:09:36 +0530

    saved in main

--- DataType.java
+++ DataType.java
@@ -1,5 +1,17 @@
 package CIE3;
 
 public class DataType {
+    public static void main(String[] args) {
+        int i = 100;
+        double d = 25.0001;
+        char c = 'v';
+        String s = "Vaishnavi";
+        boolean b = true;
 
+        System.out.println("Integer type Data : "+i);
+        System.out.println("Double type Data : "+d);
+        System.out.println("Character type Data : "+c);
+        System.out.println("String type Data : "+s); 
+        System.out.println("Boolean type Data : "+b);
+    }
 }
